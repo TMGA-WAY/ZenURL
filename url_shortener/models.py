607d042e9b8a7ec1e4f77cf11d0bb27eecaec8af
Dipankar Mitra <dipankar9612@gmail.com>
2025-01-06 20:25:35 +0530

    long url fetch and check connected with Url model

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
 
 ## URL model
 class Url(models.Model):
+    """
+    This model is used to store the long URL, short URL, user ID, and whether the URL is public or not.
+    :param short_url: Short URL
+    :param long_url: Long URL
+    :param user_id: User ID
+    :param is_public: Is the URL public or not
+    """
     short_url = models.CharField(max_length=50, unique=True)
     long_url = models.URLField(max_length=2000)
     user_id = models.IntegerField()
@@ -12,4 +20,18 @@ class Url(models.Model):
     slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
+        """
+        This method returns the string representation of the URL model.
+        :return:
+        """
         return f"Short URL: {self.short_url} \nLong URL: {self.long_url} \nUser ID: {self.user_id} \nIs Public: {self.is_public}"
+
+    def save(self, *args, **kwargs):
+        """
+        This method is used to save the URL model.
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        self.slug = slugify(self.short_url)
+        super().save(*args, **kwargs)
--- url_shortener/service/shortener_service.py
+++ url_shortener/service/shortener_service.py
@@ -38,7 +38,9 @@ def save_short_url(long_url: str, user_id: str = None, is_public: bool = True) -
 def fetch_url(url: str, user_id: str | None, is_long=True) -> str:
     try:
         # TODO: Call model to get long_url
-        url_status_string = _find_url(url=url, user_id=user_id, is_long_url=is_long)
+        url_status_string = _find_url(url=url,
+                                      user_id=user_id,
+                                      is_long_url=is_long)
 
         if url_status_string == '401':
             return _url_permission_denied(status_code=url_status_string)
@@ -56,9 +58,22 @@ def _find_url(url: str, user_id: str | None, is_long_url: bool = True) -> str:
     try:
         if is_long_url:
             # Todo: call model to fetch long_url
-            return "call model to find the short url"
-        # Todo: call model to fetch short_url
-        return "call model to find the long url"
+            url_instances = Url.objects.filter(long_url=url)
+            if not url_instances:  # If not found, then return 404
+                return '404'
+        else:
+            url_instances = Url.objects.filter(short_url=url)
+            if not url_instances:
+                return '404'
+
+        if user_id and url_instances.get("user_id", None) != user_id:
+            return '401'
+        elif url_instances.get("is_public", None) != False:
+            return '401'
+
+        return url_instances
+
+        return url_instances
     except Exception as e:
         print("Exception occurred \t", e)
         return _exception_message()
